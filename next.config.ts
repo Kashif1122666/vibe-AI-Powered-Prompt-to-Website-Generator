b5a19ffb37d69131641db4efa03e53e3f7c117d6
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-12-01 13:32:40 +0500

    20: deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
+   webpack: (config) => {
+    // Ignore your user folder to prevent EPERM errors
+    config.watchOptions = {
+      ignored: ['C:/Users/Kashif Afridi/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
